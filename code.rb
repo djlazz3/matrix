19b3364077b56e878b905372cbf0718cc48f598f
djlazz3 <Bpadron2000@gmail.com>
2014-12-03 10:01:20 -0500

    swag yolo version

--- code.rb
+++ code.rb
@@ -1,6 +1,12 @@
 require 'colorize'
-require 'win32console'
-while 1==1
-num = rand(1000000000000000000000000000000000)
-print num.to_s.green.on_black
+colors = {1 => :yellow, 2 => :blue, 3 => :green, 4 => :red}
+text = {1 => 'weed', 2 => 'sample text', 3 => 'blaze it', 4 => '420'}
+while(1==1)
+y = 1 + rand(4)
+x = colors[y]
+v = 1 + rand(4)
+r = colors[v]
+z = text[y]
+print rand(9999999999999999999999999999999999999999).to_s.colorize(:color => x, :background => r)
+print z
 end
