0567b067ad267c660685ab4d1fe071866e8d54dd
Bryan <Bpadron2000@gmail.com>
2014-12-10 17:14:51 -0500

    better matrix

--- code.rb
+++ code.rb
@@ -1,12 +1,38 @@
-require 'colorize'
-colors = {1 => :yellow, 2 => :blue, 3 => :green, 4 => :red}
-text = {1 => 'weed', 2 => 'sample text', 3 => 'blaze it', 4 => '420'}
-while(1==1)
-y = 1 + rand(4)
-x = colors[y]
-v = 1 + rand(4)
-r = colors[v]
-z = text[y]
-print rand(9999999999999999999999999999999999999999).to_s.colorize(:color => x, :background => r)
-print z
+#require 'colorize'
+#colors = {1 => :yellow, 2 => :blue, 3 => :green, 4 => :red}
+#text = {1 => 'weed', 2 => 'sample text', 3 => 'blaze it', 4 => '420'}
+x = 0
+
+while (1 == 1)
+#y = 1 + rand(4)
+#x = colors[y]
+#v = 1 + rand(4)
+#r = colors[v]
+#z = text[y]
+x += 1
+if (x <= 1)
+=begin
+print rand(2)#.to_s.colorize(:color => x, :background => r)
+elsif (x <= 10000)
+print rand(2)
+print " "
+elsif (x <= 20000)
+print rand(2)
+print "  "
+=end
+
+print rand(2)
+print rand(2)
+print rand(2)
+print rand(2)
+print "   "
+elsif (x <= 1000)
+print rand(2)
+print rand(2)
+print rand(2)
+print rand(2)
+print "    "
+x = 0
+end
+#print z
 end
